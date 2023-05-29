2e3c21719fd4c24320eafff6146c3250f9719e0d
Sebastián <sebastian.gonzalez.cass@usach.cl>
2023-05-29 09:26:19 -0400

    feat: systemRegister

--- main_21286604_CassoneGonzalez.pl
+++ main_21286604_CassoneGonzalez.pl
@@ -10,10 +10,20 @@ system(Name, System):-
     makeSystem( Name, [], [], "", "", "", [], [], Timestamp, System).
 
 % Requerimiento 2, addDrive
-addDrive(SystemOld, Letter, Name, Capacity, SystemNew):-
+% Meta Primaria: addDrive/5.
+% Meta Secundaria: makeDrive/5, makeSystem/10, verificarUnicidad/2, verificarCaracterUnico/1, addDriveInListDrive/3.
+% Dom: SystemOld, Letter, Name, Capacity, SystemNew.
+systemAddDrive(SystemOld, Letter, Name, Capacity, SystemNew):-
     makeDrive(Name, Letter, [],Capacity, NewDrive),
     makeSystem(NameSystem, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp, SystemOld),
     \+ verificarUnicidad(Letter, Drives),
     verificarCaracterUnico(Letter),
     addDriveInListDrive(Drives, NewDrive, NewListDrives),
     makeSystem(NameSystem, Users, NewListDrives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp, SystemNew).
+
+% Requerimiento 3, register
+systemRegister(SystemOld, Name, SystemNew):-
+    makeSystem(NameSystem, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp, SystemOld),
+    \+ verificarUnicidadUsuarios(Name, Users),
+    addUsuarioInListUsuario(Users, Name, NewListUsers),
+    makeSystem(NameSystem, NewListUsers, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp, SystemNew).
--- system_21286604_CassoneGonzalez.pl
+++ system_21286604_CassoneGonzalez.pl
@@ -1,4 +1,4 @@
-:- module(system_21286604_CassoneGonzalez, [makeSystem/10]).
+:- module(system_21286604_CassoneGonzalez, [makeSystem/10, verificarUnicidadUsuarios/2, addUsuarioInListUsuario/3]).
 
 % TDA System = Name x Users x Drives x CurrentUser x CurrentDrive x Path x Folders x Trash x CreationDateSystem.
 
@@ -6,3 +6,23 @@
 % Meta primaria: makeSystem/9
 % Dom: Name x Users x Drives x CurrentUser x CurrentDrive x Path x Folders x Trash x System.
 makeSystem(Name, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp, [Name, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp]).
+
+
+% Selectores
+
+% Modificadores
+
+% Verificar que el usuario sea único
+% Meta Primaria: verificarUnicidadUsuarios/2
+% Dom: Usuario x ListaUsuarios
+verificarUnicidadUsuarios(Usuario, ListaUsuarios):-
+    member(Usuario, ListaUsuarios).
+
+
+% Agregar un Usuario a una lista de Usuarios
+% Meta Primaria: addUsuarioInListUsuario/3
+% Dom: Usuario x ListUsuario x NewListUsuario
+addUsuarioInListUsuario(Usuario, ListUsuario, NewListUsuario):-
+    append(Usuario, [ListUsuario], NewListUsuario).
+
+% Pertenencia
