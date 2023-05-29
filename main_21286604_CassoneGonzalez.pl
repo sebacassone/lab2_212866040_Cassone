dd1cfd29a7a8d2838db1f26c5eb06b65eac210be
Sebastián <sebastian.gonzalez.cass@usach.cl>
2023-05-29 05:14:14 -0400

    fix: creation date

--- main_21286604_CassoneGonzalez.pl
+++ main_21286604_CassoneGonzalez.pl
@@ -2,7 +2,8 @@
 
 % Requerimiento 1, Constructor
 % Meta Primaria: system/2.
-% Meta Secundaria: makeSystem/9.
+% Meta Secundaria: makeSystem/10.
 % Dom: Name, System.
 system(Name, System):-
-    makeSystem( Name, [], [], "", "", "", [], [], System).
+    get_time(Timestamp),
+    makeSystem( Name, [], [], "", "", "", [], [], Timestamp, System).
--- system_21286604_CassoneGonzalez.pl
+++ system_21286604_CassoneGonzalez.pl
@@ -1,8 +1,8 @@
-:- module(system_21286604_CassoneGonzalez, [makeSystem/9]).
+:- module(system_21286604_CassoneGonzalez, [makeSystem/10]).
 
-% TDA System = Name x Users x Drives x CurrentUser x CurrentDrive x Path x Folders x Trash.
+% TDA System = Name x Users x Drives x CurrentUser x CurrentDrive x Path x Folders x Trash x CreationDateSystem.
 
 % Constructores
 % Meta primaria: makeSystem/9
 % Dom: Name x Users x Drives x CurrentUser x CurrentDrive x Path x Folders x Trash x System.
-makeSystem(Name, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, [Name, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash]).
+makeSystem(Name, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp, [Name, Users, Drives, CurrentUser, CurrentDrive, Path, Folders, Trash, Timestamp]).
